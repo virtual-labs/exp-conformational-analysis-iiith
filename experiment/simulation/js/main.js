82da74e219ff6c8d0f69e4c0cb9b4ef0ffbeaff7
Shlok Pandey <31105595+b30wulffz@users.noreply.github.com>
2021-08-19 16:54:28 +0530

    Added simulation  (#3)
    * added content
    
    * Add molecule and graph
    
    * fixed workflow and responsiveness
    
    * modularize JS, clean CSS using Virtual Style, add instructions
    
    * use strict mode, fix const, ===, !==
    
    * Add aim, pretest, posttest, theory. Tweak references. Delete objective
    
    * Add procedure
    
    * Fix grammar issues
    
    * Bump node version
    
    * move chemdoodle to cdn
    
    Co-authored-by: sravanthimodepu <sravanthimodepu@gmail.com>

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,150 @@
-//Your JavaScript goes in here
+"use strict";
+
+import data from "./data.js";
+
+const setMolecule = (angle, animate = false) => {
+  let width = 400;
+  if (window.innerWidth < 900) {
+    width = window.innerWidth;
+  }
+  const movie = new ChemDoodle.MovieCanvas3D("movie", width, 400);
+  if (data[angle] !== null) {
+    if (animate) {
+      for (let angle = -180; angle <= 180; angle += 3) {
+        movie.addFrame([ChemDoodle.readXYZ(data[angle]["geometry"])], []);
+      }
+    } else {
+      movie.addFrame([ChemDoodle.readXYZ(data[angle]["geometry"])], []);
+    }
+
+    movie.styles.set3DRepresentation("Ball and Stick");
+    movie.styles.atoms_displayLabels_3D = true;
+    movie.styles.backgroundColor = "transparent";
+    movie.loadMolecule(movie.frames[0].mols[0]);
+    if (animate) {
+      movie.startAnimation();
+    }
+  }
+  return movie;
+};
+
+const initChart = () => {
+  const xyValues = Object.keys(data).map((key) => ({
+    x: key,
+    y: data[key]["energy"],
+  }));
+
+  return new Chart("myChart", {
+    type: "scatter",
+    data: {
+      datasets: [
+        {
+          pointRadius: 4,
+          pointBackgroundColor: "rgb(0,0,255)",
+          pointBorderColor: "rgb(133, 193, 233)",
+          data: xyValues,
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Angle (in degree)",
+            },
+          },
+        ],
+
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Energy (Ha)",
+            },
+          },
+        ],
+      },
+    },
+  });
+};
+
+const highlightChart = (angle) => {
+  chart.data.datasets[0].pointBackgroundColor = [];
+  chart.data.datasets[0].pointRadius = [];
+  for (var i = 0; i < chart.data.datasets[0].data.length; i++) {
+    if (chart.data.datasets[0].data[i]["x"] === angle) {
+      chart.data.datasets[0].pointRadius[i] = 5;
+      chart.data.datasets[0].pointBackgroundColor[i] = "red";
+    } else {
+      chart.data.datasets[0].pointBackgroundColor[i] = "white";
+    }
+  }
+  chart.update();
+  // update dom values
+  document.getElementById("angle").innerHTML = angle;
+  document.getElementById("energy").innerHTML = data[angle]["energy"];
+  document.getElementById("angle-slider").value = angle;
+};
+
+const setAngle = (angle) => {
+  setMolecule(angle);
+  highlightChart(angle);
+};
+
+// init
+const chart = initChart();
+setAngle(document.getElementById("angle-slider").value);
+let stopAnim = true;
+
+const stopAnimation = () => {
+  stopAnim = true;
+};
+
+const playLoop = async () => {
+  const movie = setMolecule(-180, true);
+  let angle = -180;
+  let reverse = false;
+  while (true) {
+    highlightChart(angle);
+    await new Promise((r) => setTimeout(r, 56));
+    if (angle === 180) {
+      reverse = true;
+    }
+    if (!reverse) {
+      angle += 3;
+    } else {
+      angle -= 3;
+      if (angle < -180) {
+        animateButton.innerHTML = "Start Animation";
+        stopAnim = true;
+      }
+    }
+    if (stopAnim) {
+      movie.stopAnimation();
+      break;
+    }
+  }
+};
+
+// event listeners
+const angleSlider = document.getElementById("angle-slider");
+angleSlider.addEventListener("input", () => {
+  if (stopAnim) {
+    setAngle(angleSlider.value);
+  }
+});
+
+const animateButton = document.getElementById("animate-button");
+animateButton.addEventListener("click", () => {
+  if (stopAnim) {
+    animateButton.innerHTML = "Stop Animation";
+    stopAnim = false;
+    playLoop();
+  } else {
+    animateButton.innerHTML = "Start Animation";
+    stopAnim = true;
+  }
+});
